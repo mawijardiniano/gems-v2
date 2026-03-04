866248b41e8e98b9dca902430f89d507430b6bb3
Mawi <jardinianomarklouisse@gmail.com>
2026-03-04 11:29:30 +0800

    update form

--- app/(pages)/(admin)/sample-user-list/page.jsx
+++ app/(pages)/(admin)/sample-user-list/page.jsx
@@ -1,20 +1,20 @@
-// import UserListPageContent from "./content";
-// import { cookies } from "next/headers";
+import UserListPageContent from "./content";
+import { cookies } from "next/headers";
 
-// export default async function UsersListPage() {
-//   const cookieStore = await cookies();
-//   const authToken = cookieStore.get("auth_token")?.value;
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/profile`, {
-//     headers: {
-//       Cookie: `auth_token=${authToken}`,
-//     },
-//   });
+export default async function UsersListPage() {
+  const cookieStore = await cookies();
+  const authToken = cookieStore.get("auth_token")?.value;
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/profile`, {
+    headers: {
+      Cookie: `auth_token=${authToken}`,
+    },
+  });
 
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch users");
-//   }
+  if (!res.ok) {
+    throw new Error("Failed to fetch users");
+  }
 
-//   const data = await res.json();
+  const data = await res.json();
 
-//   return <UserListPageContent users={data.data} />;
-// }
+  return <UserListPageContent users={data.data} />;
+}
