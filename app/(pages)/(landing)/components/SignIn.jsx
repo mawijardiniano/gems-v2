653ee8822535faf59bcb173376a3b5b3efa6a815
Mawi <jardinianomarklouisse@gmail.com>
2026-05-20 21:19:26 +0800

    update event management system

--- app/(pages)/(landing)/components/SignIn.jsx
+++ app/(pages)/(landing)/components/SignIn.jsx
@@ -106,23 +106,7 @@ export default function LoginForm() {
     }
   };
 
-  const handleAdmin = async () => {
-    setUsername("Admin");
-    setPassword("Admin123!");
-  };
-  const handleFocal = async () => {
-    setUsername("Focal");
-    setPassword("focal123!");
-  };
-  const handleUser = async () => {
-    setUsername("JardinianoMarkLouisse");
-    setPassword("gems1234");
-  };
 
-  const handleCoordinator = async () => {
-    setUsername("DeLuna-MalinaoEunice");
-    setPassword("gems1234");
-  };
 
   return (
     <div className=" px-4 sm:px-6 md:px-8">
@@ -216,7 +200,7 @@ export default function LoginForm() {
               Sign in
             </button>
 
-            <div className="flex flex-col gap-2">
+            {/* <div className="flex flex-col gap-2">
               <h1 className="text-sm font-medium">Quick SignIn</h1>
               <div className="grid grid-cols-2 gap-2">
                 <button
@@ -244,7 +228,7 @@ export default function LoginForm() {
                   GAD Coordinator
                 </button>
               </div>
-            </div>
+            </div> */}
 
             <div className="flex justify-center">
               <p className="text-sm">
